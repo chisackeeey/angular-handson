7b1467015c78fa157d2672f5b4321f4cdeb4b0e1
chisackeeey <chisaki_h@outlook.jp>
2020-05-15 14:24:33 +0900

    mock値のリスト表示

---
+++ src/app/mock-todolist.ts
@@ -0,0 +1,7 @@
+import { Todo } from './todo';
+
+export const DEFAULT_TODO: Todo[] = [
+  { id: 1, title: 'HelloWorldを作る', done: true },
+  { id: 2, title: 'Counterを作る', done: true },
+  { id: 3, title: 'TodoListを作る', done: false },
+];
--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { DEFAULT_TODO } from './../mock-todolist';
 
 @Component({
   selector: 'app-todolist',
   templateUrl: './todolist.component.html',
-  styleUrls: ['./todolist.component.css']
+  styleUrls: ['./todolist.component.css'],
 })
 export class TodolistComponent implements OnInit {
+  todoList = DEFAULT_TODO;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
