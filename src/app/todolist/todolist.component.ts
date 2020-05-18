9765b444b7498d806cad63cb993ae1e6ef80315c
chisackeeey <chisaki_h@outlook.jp>
2020-05-18 12:33:33 +0900

    newTodoの管理をjsからhtmlに移す

--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -8,23 +8,17 @@ import { Todo } from './../todo';
   styleUrls: ['./todolist.component.css'],
 })
 export class TodolistComponent implements OnInit {
-  newTodo = '';
   todoList = DEFAULT_TODO;
 
   toggleComplete(todo: Todo) {
     todo.done = !todo.done;
   }
 
-  addTodo() {
+  addTodo(newTodo: string) {
     this.todoList = [
       ...this.todoList,
-      { id: this.todoList.length + 1, title: this.newTodo, done: false },
+      { id: this.todoList.length + 1, title: newTodo, done: false },
     ];
-    this.initialize();
-  }
-
-  initialize() {
-    this.newTodo = '';
   }
 
   constructor() {}
