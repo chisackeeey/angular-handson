18b834bce4e1de596a8260d9bde4a5c4a3607feb
chisackeeey <chisaki_h@outlook.jp>
2020-05-19 11:51:00 +0900

    buttonクリック時の処理を追加

--- src/app/translate/translate.component.ts
+++ src/app/translate/translate.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-translate',
   templateUrl: './translate.component.html',
-  styleUrls: ['./translate.component.css']
+  styleUrls: ['./translate.component.css'],
 })
 export class TranslateComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  onClick(word: string) {
+    alert(word);
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
