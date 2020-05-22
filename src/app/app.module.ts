aa0faa10c66639c597068080a14c2c9afe37c9f8
chisackeeey <chisaki_h@outlook.jp>
2020-05-22 17:47:10 +0900

    Revert "ngrxインストール、初期設定"
    This reverts commit d99621fd44889c912a22fc27eea68474fe1802fd.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './state';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule, StoreModule.forRoot(reducers, {
-      metaReducers, 
-      runtimeChecks: {
-        strictStateImmutability: true,
-        strictActionImmutability: true,
-      }
-    }), !environment.production ? StoreDevtoolsModule.instrument() : []],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
