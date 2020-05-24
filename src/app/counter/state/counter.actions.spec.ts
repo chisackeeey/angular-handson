7f2fbd9f000d9447b4f8f190c11c47da6cbd48be
chisackeeey <chisaki_h@outlook.jp>
2020-05-25 08:44:37 +0900

    不要なテストクラスの削除

--- src/app/counter/state/counter.actions.spec.ts
+++
@@ -1,7 +0,0 @@
-import * as CounterActions from './counter.actions';
-
-describe('Counter', () => {
-  it('should create an instance', () => {
-    expect(new CounterActions.LoadCounters()).toBeTruthy();
-  });
-});
