5ce9f6a5743400765b6b3bab9a0f3dcd9638a292
chisackeeey <chisaki_h@outlook.jp>
2020-05-25 13:10:19 +0900

    stateの作成

--- src/app/todolist/todolist.module.ts
+++ src/app/todolist/todolist.module.ts
@@ -3,9 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { TodolistRoutingModule } from './todolist-routing.module';
 import { TodolistComponent } from './todolist.component';
+import { StoreModule } from '@ngrx/store';
+import * as fromTodolist from './state/todolist.state';
 
 @NgModule({
   declarations: [TodolistComponent],
-  imports: [CommonModule, TodolistRoutingModule],
+  imports: [
+    CommonModule,
+    TodolistRoutingModule,
+    StoreModule.forFeature(
+      fromTodolist.todolistFeatureKey,
+      fromTodolist.reducers,
+      { metaReducers: fromTodolist.metaReducers }
+    ),
+  ],
 })
 export class TodolistModule {}
