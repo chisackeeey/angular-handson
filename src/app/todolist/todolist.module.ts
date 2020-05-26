5d996472083cc78135030783386ef715516eff78
chisackeeey <chisaki_h@outlook.jp>
2020-05-26 13:38:17 +0900

    moduleの修正

--- src/app/todolist/todolist.module.ts
+++ src/app/todolist/todolist.module.ts
@@ -1,21 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
 
 import { TodolistRoutingModule } from './todolist-routing.module';
 import { TodolistComponent } from './todolist.component';
-import { StoreModule } from '@ngrx/store';
 import * as fromTodolist from './state/todolist.state';
+import { todolistReducer } from './state/todolist.reducers';
 
 @NgModule({
   declarations: [TodolistComponent],
   imports: [
     CommonModule,
     TodolistRoutingModule,
-    StoreModule.forFeature(
-      fromTodolist.todolistFeatureKey,
-      fromTodolist.reducers,
-      { metaReducers: fromTodolist.metaReducers }
-    ),
+    StoreModule.forFeature(fromTodolist.todolistFeatureName, todolistReducer),
   ],
 })
 export class TodolistModule {}
