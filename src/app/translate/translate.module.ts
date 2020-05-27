f3ce25c0889b941103c6d1e13d7d22ea46148cce
chisackeeey <chisaki_h@outlook.jp>
2020-05-27 09:51:23 +0900

    Moduleの修正

--- src/app/translate/translate.module.ts
+++ src/app/translate/translate.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
 
 import { TranslateRoutingModule } from './translate-routing.module';
 import { TranslateComponent } from './translate.component';
+import { translateReducer } from './state/translate.reducer';
+import { translateFeatureName } from './state/translate.state';
 
 @NgModule({
   declarations: [TranslateComponent],
-  imports: [CommonModule, TranslateRoutingModule],
+  imports: [
+    CommonModule,
+    TranslateRoutingModule,
+    StoreModule.forFeature(translateFeatureName, translateReducer),
+  ],
 })
 export class TranslateModule {}
