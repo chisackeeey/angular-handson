98574cd1d0f4c9ac65e32784616a26bc51797493
chisackeeey <chisaki_h@outlook.jp>
2020-05-27 12:58:32 +0900

    コンストラクタの定義を削除

--- src/app/todolist/domain/todo.ts
+++ src/app/todolist/domain/todo.ts
@@ -1,9 +1,5 @@
 export class Todo {
-  id?: number;
-  title?: string;
-  done?: boolean;
-
-  constructor(init?: Partial<Todo>) {
-    Object.assign(this, init);
-  }
+  id: number;
+  title: string;
+  done: boolean;
 }
--- src/app/todolist/state/todolist.reducers.ts
+++ src/app/todolist/state/todolist.reducers.ts
@@ -1,7 +1,6 @@
 import { createReducer, on, Action } from '@ngrx/store';
 import { TodolistState, initialState } from './todolist.state';
 import * as TodolistActions from './todolist.actions';
-import { Todo } from '../domain/todo';
 
 export const reducer = createReducer(
   initialState,
@@ -9,14 +8,14 @@ export const reducer = createReducer(
     ...state,
     todolist: [
       ...state.todolist,
-      new Todo({ id: state.todolist.length, title, done: false }),
+      { id: state.todolist.length, title, done: false },
     ],
   })),
   on(TodolistActions.toggleComplete, (state, { id }) => ({
     ...state,
     todolist: state.todolist.map((todo) => {
       if (todo.id === id) {
-        return new Todo({ id: todo.id, title: todo.title, done: !todo.done });
+        return { id: todo.id, title: todo.title, done: !todo.done };
       } else {
         return todo;
       }
