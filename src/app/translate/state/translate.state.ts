2249133a5d851b60d09547fee54b13f19938d6fc
chisackeeey <chisaki_h@outlook.jp>
2020-05-28 10:53:32 +0900

    Stateの修正

--- src/app/translate/state/translate.state.ts
+++ src/app/translate/state/translate.state.ts
@@ -1,11 +1,27 @@
-import {} from '@ngrx/store';
+import { createSelector, createFeatureSelector } from '@ngrx/store';
 
 export interface TranslateState {
   result: string;
+  loading: boolean;
 }
 
 export const initialState: TranslateState = {
   result: '',
+  loading: false,
 };
 
 export const translateFeatureName = 'translate';
+
+export const selectTranslateFeature = createFeatureSelector<TranslateState>(
+  translateFeatureName
+);
+
+export const selectResult = createSelector(
+  selectTranslateFeature,
+  (state) => state.result
+);
+
+export const selectLoading = createSelector(
+  selectTranslateFeature,
+  (state) => state.loading
+);
