00e902beccc3bc97a0123f41607608d6002c5c40
chisackeeey <chisaki_h@outlook.jp>
2020-05-28 10:53:57 +0900

    reducerの処理の実装

--- src/app/translate/state/translate.reducer.ts
+++ src/app/translate/state/translate.reducer.ts
@@ -1,11 +1,18 @@
-import { createReducer, on, Action, State } from '@ngrx/store';
+import { createReducer, on, Action } from '@ngrx/store';
 import { TranslateState, initialState } from './translate.state';
 import * as TranslateActions from './translate.actions';
 
 export const reducer = createReducer(
   initialState,
-  on(TranslateActions.translateJaToEn, (state) => ({})),
-  on(TranslateActions.translateEnToJa, (state) => ({}))
+  on(TranslateActions.translateJaToEn, (state) => {
+    return { ...state, loading: true };
+  }),
+  on(TranslateActions.translateEnToJa, (state) => {
+    return { ...state, loading: true };
+  }),
+  on(TranslateActions.setResult, (state, { result }) => {
+    return { ...state, loading: false, result };
+  })
 );
 
 export function translateReducer(state: TranslateState, action: Action) {
