d1f80b4ef41725263cd8ccbd2224e279ae792113
chisackeeey <chisaki_h@outlook.jp>
2020-06-04 07:26:19 +0900

    TodolistServiceのテストクラス作成

---
+++ src/app/todolist/service/todolist.service.spec.ts
@@ -0,0 +1,50 @@
+import { TestBed } from '@angular/core/testing';
+import { StoreModule } from '@ngrx/store';
+
+import { TodolistService } from './todolist.service';
+import { todolistFeatureName } from '../state/todolist.state';
+import { todolistReducer } from '../state/todolist.reducers';
+
+describe('TodolistService', () => {
+  let service: TodolistService;
+  TodolistService;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [{ provide: TodolistService }],
+      imports: [
+        StoreModule.forRoot({}),
+        StoreModule.forFeature(todolistFeatureName, todolistReducer),
+      ],
+    });
+    service = TestBed.inject(TodolistService);
+  });
+
+  it('addTodo() should add todo', () => {
+    const expected = [
+      {
+        id: 0,
+        title: 'add',
+        done: false,
+      },
+    ];
+    service.addTodo('add');
+    service.getTodolist().subscribe((todolist) => {
+      expect(todolist).toEqual(expected);
+    });
+  });
+
+  it('toggleComplete() should toggle complete', () => {
+    const expected = [
+      {
+        id: 0,
+        title: 'add',
+        done: true,
+      },
+    ];
+    service.addTodo('add');
+    service.toggleComplete(1);
+    service.getTodolist().subscribe((todolist) => {
+      expect(todolist).toEqual(expected);
+    });
+  });
+});
