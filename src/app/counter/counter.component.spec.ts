b0e00a71172c4d7632f528ed41b1d07418027eab
chisackeeey <chisaki_h@outlook.jp>
2020-06-03 14:02:32 +0900

    componentテスト修正

--- src/app/counter/counter.component.spec.ts
+++ src/app/counter/counter.component.spec.ts
@@ -1,7 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { BehaviorSubject } from 'rxjs';
 
 import { CounterComponent } from './counter.component';
+import { CounterService } from './service/counter.service';
 
 describe('CounterComponent', () => {
   let component: CounterComponent;
@@ -11,9 +13,14 @@ describe('CounterComponent', () => {
     count: 0,
   };
 
+  const counterServiceMock = {
+    count: new BehaviorSubject(0),
+    increment: () => void 0,
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      providers: provideMockStore({ initialState }),
+      providers: [{ provide: CounterService, useValue: counterServiceMock }],
       declarations: [CounterComponent],
     }).compileComponents();
   }));
@@ -26,6 +33,15 @@ describe('CounterComponent', () => {
   });
 
   it('should create', () => {
+    const fixture = TestBed.createComponent(CounterComponent);
+    const component = fixture.componentInstance;
     expect(component).toBeTruthy();
+  });
+
+  it(`should have point value`, () => {
+    const fixture = TestBed.createComponent(CounterComponent);
+    fixture.detectChanges();
+    const counter = fixture.debugElement.componentInstance;
+    expect(counter.count).toEqual(0);
   });
 });
