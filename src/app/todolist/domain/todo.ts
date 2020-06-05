275c0407a871d304f608054ca7854b34b7e6527e
chisackeeey <chisaki_h@outlook.jp>
2020-06-05 15:33:17 +0900

    変数宣言時に初期値を設定する

--- src/app/todolist/domain/todo.ts
+++ src/app/todolist/domain/todo.ts
@@ -1,11 +1,5 @@
 export class Todo {
-  id: number;
-  title: string;
-  done: boolean;
-
-  constructor() {
-    this.id = 0;
-    this.title = '';
-    this.done = false;
-  }
+  id? = 0;
+  title = '';
+  done = false;
 }
