af035a57583fe0cb94d191add5d35c7df645f947
chisackeeey <chisaki_h@outlook.jp>
2020-06-07 18:42:23 +0900

    update TranslateService test

--- src/app/translate/service/translate.service.spec.ts
+++ src/app/translate/service/translate.service.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { of } from 'rxjs';
+import { take, delay } from 'rxjs/operators';
 
 import { TranslateService } from './translate.service';
-import { translateFeatureName } from '../state/translate.state';
+import { TranslateApiService } from '../api/translate-api.service';
+import { TranslateEffects } from '../state/translate.effects';
+import { translateFeatureName, TranslateState } from '../state/translate.state';
 import { reducer } from '../state/translate.reducer';
 
 describe('TodolistService', () => {
@@ -13,29 +17,51 @@ describe('TodolistService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
-        { provide: HttpClient, useValue: null },
+        {
+          provide: TranslateApiService,
+          useValue: {
+            translateJaToEn: (text: string) => of('dog').pipe(delay(2000)),
+          },
+        },
         { provide: TranslateService },
       ],
       imports: [
         HttpClientTestingModule,
         StoreModule.forRoot({}),
         StoreModule.forFeature(translateFeatureName, reducer),
+        EffectsModule.forRoot([]),
+        EffectsModule.forFeature([TranslateEffects]),
       ],
     });
     service = TestBed.inject(TranslateService);
   });
 
-  it('addTodo() should add todo', () => {
-    const expected = {
-      result: 'dog',
-      loading: false,
-    };
+  it('translateJaToEn() should translate Ja to En', (done: DoneFn) => {
+    const expected: TranslateState[] = [
+      { result: '', loading: true },
+      { result: 'dog', loading: false },
+    ];
     service.translateJaToEn('犬');
-    service.getLoading().subscribe((loading) => {
-      expect(loading).toBe(expected.loading);
-    });
-    // service.getResult().subscribe((result) => {
-    //   expect(result).toBe(expected.result);
-    // });
+
+    let i = 0;
+    service
+      .getLoading()
+      .pipe(take(2))
+      .subscribe((loading) => {
+        console.log(`expect(${loading}).toBe(${expected[i].loading})`);
+        expect(loading).toBe(expected[i].loading);
+        i++;
+      });
+
+    let j = 0;
+    service
+      .getResult()
+      .pipe(take(2))
+      .subscribe((result) => {
+        console.log(`expect(${result}).toBe(${expected[j].result})`);
+        expect(result).toBe(expected[j].result);
+        j++;
+        if (j === 2) done();
+      });
   });
 });
