46cbbd0010b7b6b51cf436c624e40e40b7d7986e
chisackeeey <chisaki_h@outlook.jp>
2020-06-07 19:53:29 +0900

    update TranslateApiService test

--- src/app/translate/api/translate-api.service.spec.ts
+++ src/app/translate/api/translate-api.service.spec.ts
@@ -6,13 +6,20 @@ import { of } from 'rxjs';
 
 describe('TranslateApiService', () => {
   let service: TranslateApiService;
+  let language: 'engilsh' | 'japanese';
+  const response = { engilsh: 'dog', japanese: '犬' };
+  const expected = { engilsh: 'dog', japanese: '犬' };
+
+  const httpClientMock = {
+    get: () => of(response[language]),
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
         {
           provide: HttpClient,
-          useValue: { get: () => of('dog') },
+          useValue: httpClientMock,
         },
         { provide: TranslateApiService },
       ],
@@ -21,9 +28,18 @@ describe('TranslateApiService', () => {
   });
 
   it('translateJaToEn() should translate Ja to En', () => {
-    const expected = 'dog';
+    language = 'engilsh';
     service.translateJaToEn('犬').subscribe((result) => {
-      expect(result).toBe(expected);
+      console.log(`expect(${result}).toBe(${expected[language]})`);
+      expect(result).toBe(expected[language]);
+    });
+  });
+
+  it('translateEnToJa() should translate En to Ja', () => {
+    language = 'japanese';
+    service.translateEnToJa('dog').subscribe((result) => {
+      console.log(`expect(${result}).toBe(${expected[language]})`);
+      expect(result).toBe(expected[language]);
     });
   });
 });
