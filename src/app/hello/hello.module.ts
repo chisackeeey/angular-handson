8ede57091dd2e4429ad021f698db1aef89192015
chisackeeey <chisaki_h@outlook.jp>
2020-07-03 03:04:33 +0900

    edit hello

--- src/app/hello/hello.module.ts
+++ src/app/hello/hello.module.ts
@@ -3,9 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { HelloRoutingModule } from './hello-routing.module';
 import { HelloComponent } from './hello.component';
+import { StoreModule } from '@ngrx/store';
+import { helloReducer } from './state/hello.reducer';
+import { helloFeatureName } from './state/hello.state';
 
 @NgModule({
   declarations: [HelloComponent],
-  imports: [CommonModule, HelloRoutingModule],
+  imports: [
+    CommonModule,
+    HelloRoutingModule,
+    StoreModule.forFeature(helloFeatureName, helloReducer),
+  ],
 })
 export class HelloModule {}
